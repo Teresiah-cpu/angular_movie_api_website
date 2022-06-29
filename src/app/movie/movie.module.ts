f22de280e27708dfdee209a041758d237f27dec7
Teresiah Githua <terrywambo2018@gmail.com>
2022-06-29 15:47:25 +0300

    ft:create add movie functionality

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieRoutingModule } from './movie-routing.module';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { AddMovieComponent } from './add-movie/add-movie.component';
 
 
 
 @NgModule({
   declarations: [
-    MovieDetailComponent
+    MovieDetailComponent,
+    AddMovieComponent
   ],
   imports: [
     CommonModule,
